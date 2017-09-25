a72196243c56a8286af1c1c17e2fb0cc4cc5b309
Pranjal Srivastava <32098501+SrivastavaPranjal@users.noreply.github.com>
2017-09-25 20:41:46 +0530

    Add files via upload

---
+++ Commands/npl2.bat
@@ -0,0 +1,6 @@
+@echo off
+echo New Programming Language
+echo Made By Harshit and Pranjal Srivastava
+javac -cp .;antlr-3.2.jar NplParser.java NplLexer.java
+echo New Programming Language
+echo Made By Harshit and Pranjal Srivastava
---
+++ Commands/run.bat
@@ -0,0 +1,8 @@
+@echo off
+echo New Programming Language
+echo Made By Harshit and Pranjal Srivastava
+
+echo.
+echo.
+echo Compiling
+java -cp .;antlr-3.2.jar NplParser %*.npl
