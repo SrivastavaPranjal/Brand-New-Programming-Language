ae98b7000d6842a7df916e5c87cce8a284480dd1
Pranjal Srivastava <32098501+SrivastavaPranjal@users.noreply.github.com>
2017-09-25 20:41:13 +0530

    Create np1.bat

---
+++ Commands/np1.bat
@@ -0,0 +1,6 @@
+@echo off
+echo New Programming Language
+echo Made By Harshit and Pranjal Srivastava
+java -cp antlr-3.2.jar org.antlr.Tool Npl.g
+echo New Programming Language
+echo Made By Harshit and Pranjal Srivastava
